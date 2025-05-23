289f23c85ed3883660d3a3e0a3e1c4c37b6908fc
krentor471 <lib.ruina@gmail.com>
2025-05-24 00:00:08 +0300

    First lab done

---
+++ HW1/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Колобанов Алексей 241-321 ЛБ1</title>
+</head>
+<body>
+    <header style="display: flex; justify-content: left; gap: 5%;">
+        <img src="./logo.jpg" alt="Логотип" style="width: 30%; height: 20%;">
+        <h1>Hello, World!</h1>
+    </header>
+    <main>
+        <p><?php echo "Привет, мир!" ?></p>
+    </main>
+    <footer>
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos commodi eius dolorem tenetur officia rem qui dolorum accusantium, autem doloremque beatae fugiat blanditiis et optio, provident eligendi voluptatem quod. Cum?
+        Impedit eius harum, necessitatibus tempora totam minus fugiat porro labore. Provident cupiditate voluptate saepe laudantium numquam accusantium, quae nemo voluptates! Ad eos dolore aliquam voluptates harum eum nisi placeat accusantium.
+        Nisi assumenda illum, vero, dolor minus culpa doloremque optio aliquam ad perspiciatis voluptate in delectus, adipisci eos totam? In tempora rem quibusdam suscipit illum provident omnis at dolor blanditiis molestias!
+        Magnam beatae id quidem vitae, ea sapiente perferendis ipsam velit veritatis quos optio, deleniti porro autem! Totam, id consequuntur magni libero corporis distinctio ipsum officia repellat soluta, reprehenderit officiis fugiat.
+        Molestias quas esse tenetur dolorem. Repellat fugit excepturi rerum laborum rem, sint quis. Ratione rem magni ad hic aperiam natus, atque, culpa doloribus voluptatum praesentium blanditiis explicabo quo ducimus delectus?
+        Hic cumque voluptatem magnam veritatis tempora dolores omnis minima ipsam velit ipsum nobis autem incidunt maxime voluptate qui ipsa quos, ducimus numquam deserunt architecto error sunt odit? In, perspiciatis cumque?
+        Qui, ad natus. Aliquid distinctio optio nisi at? Nihil dolore similique modi recusandae tenetur cum fugit nostrum quaerat ad, blanditiis sapiente suscipit facere rem doloribus? Doloribus commodi quia pariatur eius.
+        Alias totam cum praesentium iste modi, illo at voluptatum cumque fugit delectus ad eum debitis omnis assumenda aliquid. Quaerat inventore reprehenderit dolores quasi magnam recusandae at voluptas expedita dignissimos eveniet?
+        Quod quo veniam reiciendis. Delectus est sapiente sequi vel, nihil ab quod reprehenderit! Recusandae, distinctio reiciendis cum eligendi totam tenetur, quidem mollitia doloremque amet temporibus veniam tempore libero natus repellendus.
+        Rem omnis sequi voluptatum optio quo. Eveniet voluptas cupiditate consequuntur nemo voluptatem corrupti, ut laborum iusto sapiente sunt obcaecati non est fuga officiis at repellat eius possimus numquam dignissimos modi!
+        Tempore, esse officia vel voluptatem totam temporibus porro culpa neque expedita corrupti? Dolor, consequatur laborum quisquam amet tempore, voluptatibus aliquam assumenda praesentium blanditiis commodi officiis ipsum maxime est, nesciunt exercitationem?
+        Dolore cupiditate alias, aperiam a error commodi molestiae autem, laudantium similique facere voluptatum deleniti enim quis culpa sit, minus obcaecati. Consectetur beatae porro illum aspernatur itaque earum a magnam repudiandae.
+        Architecto quas voluptatem quibusdam ut! Non sapiente hic quas porro ipsum provident asperiores nobis quis, quaerat, labore, reiciendis cupiditate. A, possimus veniam perferendis dignissimos asperiores minima! Soluta fugit consectetur labore.
+        Eveniet esse facere velit quis totam ab voluptas asperiores sed eum expedita, deserunt blanditiis quisquam nesciunt, nam exercitationem corporis quia doloremque quos doloribus dolore labore culpa quas vitae. Non, delectus.
+        Aliquam repudiandae ipsa porro optio beatae repellendus deleniti quia dolor magni asperiores voluptas aspernatur laudantium velit maxime hic ab, omnis quis illo atque? Sit repellendus quibusdam natus, odit ut amet.</p>
+    </footer>
+</body>
+</html>
+
